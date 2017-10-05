7d0912b48bdc18bcafb35cddb5b81beb78821487
Krishna Kalyan <krishnakalyan3@gmail.com>
2017-10-05 18:45:01 +0530

    code

--- src/models/basic_models.py
+++ src/models/basic_models.py
@@ -15,3 +15,13 @@ class Net(nn.Module):
         out = self.relu(out)
         out = self.fc2(out)
         return out
+
+
+class LinearRegression(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(LinearRegression, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+
+    def forward(self, x):
+        out = self.linear(x)
+        return out
---
+++ src/other/custom_data.py
@@ -0,0 +1,49 @@
+import torch
+import torchvision as tv
+
+
+class ImagesDataset(torch.utils.data.Dataset):
+    def __init__(self, df, transform=None,
+                 loader=tv.datasets.folder.default_loader):
+        self.df = df
+        self.transform = transform
+        self.loader = loader
+
+    def __getitem__(self, index):
+        row = self.df.iloc[index]
+
+        target = row['class_']
+        path = row['path']
+        img = self.loader(path)
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target
+
+    def __len__(self):
+        n, _ = self.df.shape
+        return n
+
+# what transformations should be done with our images
+data_transforms = tv.transforms.Compose([
+    tv.transforms.RandomCrop((64, 64), padding=4),
+    tv.transforms.RandomHorizontalFlip(),
+    tv.transforms.ToTensor(),
+])
+
+train_df = pd.read_csv('path/to/some.csv')
+# initialize our dataset at first
+train_dataset = ImagesDataset(
+    df=train_df,
+    transform=data_transforms
+)
+
+# initialize data loader with required number of workers and other params
+train_loader = torch.utils.data.DataLoader(train_dataset,
+                                           batch_size=10,
+                                           shuffle=True,
+                                           num_workers=16)
+
+# fetch the batch(call to `__getitem__` method)
+for img, target in train_loader:
+    pass
