5379c8dd914c999e11acb4f65296c459fccfe1c2
Krishna Kalyan <krishnakalyan3@gmail.com>
2017-10-05 18:26:12 +0530

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # PyTorch Experiments
 
 
+SNo.| Examples
+--- | ---
+1 | [Matrix Multiplication]()
+2 | [Feed Forward MNIST]()
+
+
 Resources 
 - [Deep Learning Course](https://fleuret.org/dlc/)
---
+++ src/01_feed_forward.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn as nn
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+import torch.nn.functional as F
+import argparse
+from models.basic_models import Net
+
+
+MNIST_DATA = '../data/MNIST'
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch FeedForward Example')
+    parser.add_argument('--epochs', type=int, default=1, help='number of epochs to train')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+    parser.add_argument('--batch_size', type=int, default=64, help='batch size')
+
+    args = parser.parse_args()
+
+    train_dataset = dsets.MNIST(root=MNIST_DATA,
+                                train=True,
+                                transform=transforms.ToTensor(),
+                                download=True)
+
+    test_dataset = dsets.MNIST(root=MNIST_DATA,
+                               train=False,
+                               transform=transforms.ToTensor())
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                               batch_size=64,
+                                               shuffle=True)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                              batch_size=64,
+                                              shuffle=False)
+
+    net = Net(input_size=784, hidden_size=500, num_classes=10)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters(), lr=args.lr)
+
+    # Train
+    for epoch in range(args.epochs):
+        for i, (images, labels) in enumerate(train_loader):
+            images = Variable(images.view(-1, 28 * 28))
+            labels = Variable(labels)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            print('Epoch [%d/%d], Step [%d/%d], Loss: %.4f'% (epoch + 1, args.epochs, i + 1, len(train_dataset) // args.batch_size,
+                                                              loss.data[0]))
+
+    # Test
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = Variable(images.view(-1, 28 * 28))
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum()
+        for i in range(len(labels)):
+            if labels[i] != predicted[i]:
+                print('True Label {}, Predict Label {}'.format(labels[i], predicted[i]))
+
+    print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
