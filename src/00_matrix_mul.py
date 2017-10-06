8130881926e3eedb05c6d8a6328df10b5466a1b4
Krishna Kalyan <krishnakalyan3@gmail.com>
2017-10-06 04:08:07 +0400

    plot stuff

--- src/00_matrix_mul.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import torch
-
-a = torch.IntTensor([1, 2, 3])
-b = torch.IntTensor([4, 5, 6])
-m = a * b
-print(a.numpy())
-print(m.numpy())
--- src/03_logistic_regression.py
+++ src/03_logistic_regression.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # Please download the housing dataset from https://www.kaggle.com/apratim87/housingdata
 
+import matplotlib
+matplotlib.use('TkAgg')
 import torch
 import torch.nn as nn
 import argparse
@@ -9,12 +11,14 @@ import matplotlib.pyplot as plt
 from models.basic_models import LogisticRegression
 import torchvision.transforms as transforms
 import torchvision.datasets as dsets
+import numpy as np
+
 
 MNIST_DATA = '../data/MNIST'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PyTorch FeedForward Example')
-    parser.add_argument('--epochs', type=int, default=2, help='number of epochs to train')
+    parser.add_argument('--epochs', type=int, default=1, help='number of epochs to train')
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
     parser.add_argument('--batch_size', type=int, default=64, help='batch size')
     args = parser.parse_args()
@@ -65,6 +69,11 @@ if __name__ == '__main__':
 
             # Print Incorrect Images
             if labels[i] != predicted[i]:
+                current_image = images.data.numpy()[i].reshape(-1, 28)
+                plt.clf()
+                plt.imshow(current_image, cmap='gray_r', )
+                plt.show(block=False)
+                plt.pause(0.10)
                 print('True Label {}, Predict Label {}'.format(labels[i], predicted[i]))
 
     print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
