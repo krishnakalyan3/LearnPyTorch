66f8780f2df3fe5a04ea1a516aa7bcc16eb75ada
Krishna Kalyan <krishnakalyan3@gmail.com>
2017-11-27 01:28:27 +0100

    cifar10

---
+++ src/07_cifar.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import torchvision
+import torchvision.transforms as transforms
+import os
+import argparse
+from torch.autograd import Variable
+from models import *
+
+
+def data_loader():
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+    train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
+
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+    test_loader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
+
+    return train_loader, test_loader
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch FeedForward Example')
+    parser.add_argument('--epochs', type=int, default=20, help='number of epochs to train')
+    parser.add_argument('--lr', type=float, default=0.1, help='learning rate')
+    args = parser.parse_args()
+
+    use_cuda = torch.cuda.is_available()
+    train_loader, test_loader = data_loader()
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+    net = SENet18()
+
+    if use_cuda:
+        net.cuda()
+        net = torch.nn.DataParallel(net, device_ids=range(torch.cuda.device_count()))
+
